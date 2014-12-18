dc0052e974914017caed3649518d02706c9d1f94
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2014-12-18 15:47:08 +0000

    Basic spacial rules

--- src/paper/core.clj
+++ src/paper/core.clj
@@ -56,10 +56,140 @@
 ;;  - z must be positive
 (defn paper-pos [p x y z]
   (all (paper p)
-       (fd/>= 0)))
+       (fd/>= z 0)))
 
-;; Properties
-(db-rel seam p1 p2)
+;; Object with the same z value exists on the same plane
+(defn same-plane [p1 p2]
+  (all (paper p1)
+       (paper p2)
+       (!= p1 p2)
+       (fresh [p1-x p1-y p1-z
+               p2-x p2-y p2-z]
+         (paper-pos p1 p1-x p1-y p1-z)
+         (paper-pos p2 p2-x p2-y p2-z)
+         (fd/== p1-z p2-z))))
 
-(db-rel on p-top p-bottom)
-(db-rel over p-top p-bottom)
+(defn different-plane [p1 p2]
+  (all (paper p1)
+       (paper p2)
+       (!= p1 p2)
+       (fresh [p1-x p1-y p1-z
+               p2-x p2-y p2-z]
+         (paper-pos p1 p1-x p1-y p1-z)
+         (paper-pos p2 p2-x p2-y p2-z)
+         (fd/!= p1-z p2-z))))
+
+;; Or they can be above or below each other
+(defn above [p1 p2]
+  (all (paper p1)
+       (paper p2)
+       (!= p1 p2)
+       (fresh [p1-x p1-y p1-z
+               p2-x p2-y p2-z]
+         (paper-pos p1 p1-x p1-y p1-z)
+         (paper-pos p2 p2-x p2-y p2-z)
+         (fd/> p1-z p2-z))))
+
+(defn below [p1 p2]
+  (all (paper p1)
+       (paper p2)
+       (!= p1 p2)
+       (fresh [p1-x p1-y p1-z
+               p2-x p2-y p2-z]
+         (paper-pos p1 p1-x p1-y p1-z)
+         (paper-pos p2 p2-x p2-y p2-z)
+         (fd/< p1-z p2-z))))
+
+;; They can intersect
+(defn intersect [p1 p2]
+  (all (paper p1)
+       (paper p2)
+       (!= p1 p2)
+       (fresh [p1-x p1-y p1-z
+               p1-w p1-h p1-d
+               p2-x p2-y p2-z
+               p2-w p2-h p2-d]
+         (paper-dims p1 p1-w p1-h p1-d)
+         (paper-dims p2 p2-w p2-h p2-d)
+         (paper-pos p1 p1-x p1-y p1-z)
+         (paper-pos p2 p2-x p2-y p2-z)
+         (fresh [p1-x+w p1-y+h p2-x+w p2-y+h]
+           (fd/+ p1-x p1-w p1-x+w)
+           (fd/+ p1-y p1-h p1-y+h)
+           (fd/+ p2-x p2-w p2-x+w)
+           (fd/+ p2-y p2-h p2-y+h)
+           (fd/< p1-x p2-x+w)
+           (fd/< p2-x p1-x+w)
+           (fd/< p1-y p2-y+h)
+           (fd/< p2-y p1-y+h)))))
+
+;; One can be inside another
+(defn inside [p1 p2]
+  (all (paper p1)
+       (paper p2)
+       (!= p1 p2)
+       (fresh [p1-x p1-y p1-z
+               p1-w p1-h p1-d
+               p2-x p2-y p2-z
+               p2-w p2-h p2-d]
+         (paper-dims p1 p1-w p1-h p1-d)
+         (paper-dims p2 p2-w p2-h p2-d)
+         (paper-pos p1 p1-x p1-y p1-z)
+         (paper-pos p2 p2-x p2-y p2-z)
+         (fresh [p1-x+w p1-y+h p2-x+w p2-y+h]
+           (fd/+ p1-x p1-w p1-x+w)
+           (fd/+ p1-y p1-h p1-y+h)
+           (fd/+ p2-x p2-w p2-x+w)
+           (fd/+ p2-y p2-h p2-y+h)
+           (fd/>= p1-x p2-x)
+           (fd/>= p2-x+w p1-x+w)
+           (fd/>= p1-y p2-y)
+           (fd/>= p2-y+h p1-y+h)))))
+
+;; Multiple material elements cannot occupy the same point in space simultaneously.
+;;
+;; In physics this is the Pauli exclusion principle
+(defn pauli [p1 p2]
+  (conde
+   [(== p1 p2)]
+   [(intersect p1 p2) (different-plane p1 p2)]
+   [(!intersect p1 p2)]))
+
+;; Containers and the objects they contain have a parent-child
+;; relationship. Every object has a single parent, and may or may not
+;; have one or more children
+(db-rel parent container object)
+
+;; Or it can be the root
+(db-rel root object)
+
+;; Objects can be placed on a container
+(defn on-container [container object]
+  (all (parent container object)
+       (above object container)
+       (inside object container)))
+
+;; Materials may be at rest or raised
+(db-rel at-rest p)
+(db-rel raised p)
+
+;; They also have a rest elevation
+(db-rel rest-elevation p z)
+
+;; If they live on a container they are raised to the height of the container
+(defn rest-z [p z]
+  (fresh [parent z-rest]
+    (rest-elevation p z-rest)
+    (conde [(on-container parent p) (fresh [c-x c-y c-z]
+                                      (paper-pos c-x c-y c-z)
+                                      (fd/+ c-z z-rest z))]
+           [(root p) (z-rest z)])))
+
+;; At rest paper adopts its rest elevation, if raised it it moves up by 6dp
+(defn elevation [p z]
+  (fresh [x y z-rest]
+    (paper-pos p x y z)
+    (rest-z p z-rest)
+    (conde
+     [(at-rest p) (== z z-rest)]
+     [(raised p) (fd/+ z-rest 6 z)])))
